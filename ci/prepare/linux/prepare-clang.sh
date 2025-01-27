e056dd795c79bfaf72c5a9247c8b1197cfe091b3
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2025-01-27 21:04:21 +0100

    Restore workaround

--- ci/prepare/linux/prepare-clang.sh
+++ ci/prepare/linux/prepare-clang.sh
@@ -12,3 +12,7 @@ curl -O https://apt.llvm.org/llvm.sh && chmod +x llvm.sh && ./llvm.sh $CLANG_VER
 update-alternatives --install /usr/bin/cc cc /usr/bin/clang-$CLANG_VERSION 100
 update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-$CLANG_VERSION 100
 update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-$CLANG_VERSION 100
+
+# Workaround to get a path that can be easily passed into cmake for BOOST_STACKTRACE_BACKTRACE_INCLUDE_FILE
+# See https://www.boost.org/doc/libs/1_70_0/doc/html/stacktrace/configuration_and_build.html#stacktrace.configuration_and_build.f3
+backtrace_file=$(find /usr/lib/gcc/ -name 'backtrace.h' | head -n 1) && test -f $backtrace_file && ln -s $backtrace_file /usr/local/include/backtrace.h
