14db0d483e1bd22aa258758cc41a54baac8920d4
Gustav Schauwecker <service@simpago.com>
2025-01-31 11:24:34 +0100

    Rename ResponseHandler to ResponseProcessor

--- node/src/bootstrap/response_processor/mod.rs
+++ node/src/bootstrap/response_processor/mod.rs
@@ -1,2 +1,2 @@
-mod response_handler;
-pub(crate) use response_handler::{BootstrapProcessError, ResponseHandler};
+mod response_processor;
+pub(crate) use response_processor::{BootstrapProcessError, ResponseHandler};
