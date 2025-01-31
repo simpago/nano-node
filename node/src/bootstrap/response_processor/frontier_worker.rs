deadf94f1bd4554140e37ff206b300ffcdd9c669
Gustav Schauwecker <service@simpago.com>
2025-01-31 16:22:08 +0100

    Add tests for FrontierWorker

--- node/src/bootstrap/response_processor/frontier_worker.rs
+++ node/src/bootstrap/response_processor/frontier_worker.rs
@@ -1,4 +1,4 @@
-use super::frontier_checker::FrontierChecker;
+use super::frontier_checker::{FrontierChecker, OutdatedAccounts};
 use crate::{
     bootstrap::state::{BootstrapState, CandidateAccounts},
     stats::{DetailType, StatType, Stats},
@@ -7,7 +7,6 @@ use rsnano_core::Frontier;
 use rsnano_ledger::Ledger;
 use rsnano_store_lmdb::LmdbReadTransaction;
 use std::sync::Mutex;
-use tracing::debug;
 
 /// Handles received frontiers
 pub(crate) struct FrontierWorker<'a> {
@@ -31,13 +30,18 @@ impl<'a> FrontierWorker<'a> {
     }
 
     pub fn process(&mut self, frontiers: Vec<Frontier>) {
-        assert!(!frontiers.is_empty());
-
-        self.stats
-            .inc(StatType::Bootstrap, DetailType::ProcessingFrontiers);
-
         let outdated = self.checker.get_outdated_accounts(&frontiers);
+        self.update_stats(&frontiers, &outdated);
+        let mut guard = self.state.lock().unwrap();
+        for account in outdated.accounts {
+            // Use the lowest possible priority here
+            guard
+                .candidate_accounts
+                .priority_set(&account, CandidateAccounts::PRIORITY_CUTOFF);
+        }
+    }
 
+    fn update_stats(&self, frontiers: &[Frontier], outdated: &OutdatedAccounts) {
         self.stats.add(
             StatType::BootstrapFrontiers,
             DetailType::Processed,
@@ -58,20 +62,51 @@ impl<'a> FrontierWorker<'a> {
             DetailType::Pending,
             outdated.pending as u64,
         );
+    }
+}
 
-        debug!(
-            "Processed {} frontiers of which outdated: {}, pending: {}",
-            frontiers.len(),
-            outdated.outdated,
-            outdated.pending
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rsnano_core::{Account, AccountInfo, BlockHash};
+
+    #[test]
+    fn empty() {
+        let ledger = Ledger::new_null();
+        let tx = ledger.read_txn();
+        let stats = Stats::default();
+        let state = Mutex::new(BootstrapState::default());
+        let mut worker = FrontierWorker::new(&ledger, &tx, &stats, &state);
+
+        worker.process(Vec::new());
+
+        assert_eq!(state.lock().unwrap().candidate_accounts.priority_len(), 0);
+    }
+
+    #[test]
+    fn prioritize_one_account() {
+        let account = Account::from(1);
+        let ledger = Ledger::new_null_builder()
+            .account_info(
+                &account,
+                &AccountInfo {
+                    head: BlockHash::from(2),
+                    ..Default::default()
+                },
+            )
+            .finish();
+        let tx = ledger.read_txn();
+        let stats = Stats::default();
+        let state = Mutex::new(BootstrapState::default());
+        let mut worker = FrontierWorker::new(&ledger, &tx, &stats, &state);
+
+        worker.process(vec![Frontier::new(account, BlockHash::from(3))]);
+
+        let guard = state.lock().unwrap();
+        assert_eq!(guard.candidate_accounts.priority_len(), 1);
+        assert_eq!(
+            guard.candidate_accounts.priority(&account),
+            CandidateAccounts::PRIORITY_CUTOFF
         );
-
-        let mut guard = self.state.lock().unwrap();
-        for account in outdated.accounts {
-            // Use the lowest possible priority here
-            guard
-                .candidate_accounts
-                .priority_set(&account, CandidateAccounts::PRIORITY_CUTOFF);
-        }
     }
 }
