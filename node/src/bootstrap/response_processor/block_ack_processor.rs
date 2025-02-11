fac77132cc7a1ccb463d718ebf89de268bf200b3
Gustav Schauwecker <service@simpago.com>
2025-02-11 16:27:01 +0100

    Tidy

--- node/src/bootstrap/response_processor/block_ack_processor.rs
+++ node/src/bootstrap/response_processor/block_ack_processor.rs
@@ -33,81 +33,11 @@ impl BlockAckProcessor {
         let result = query.verify_blocks(response);
         match result {
             VerifyResult::Ok => {
-                self.stats
-                    .inc(StatType::BootstrapVerifyBlocks, DetailType::Ok);
-                self.stats.add_dir(
-                    StatType::Bootstrap,
-                    DetailType::Blocks,
-                    Direction::In,
-                    response.blocks().len() as u64,
-                );
-
-                let mut blocks = response.blocks().clone();
-
-                // Avoid re-processing the block we already have
-                assert!(blocks.len() >= 1);
-                if blocks.front().unwrap().hash() == query.start.into() {
-                    blocks.pop_front();
-                }
-
-                while let Some(block) = blocks.pop_front() {
-                    if blocks.is_empty() {
-                        // It's the last block submitted for this account chain, reset timestamp to allow more requests
-                        let stats = self.stats.clone();
-                        let state = self.state.clone();
-                        let account = query.account;
-                        self.block_processor.add_with_callback(
-                            block,
-                            BlockSource::Bootstrap,
-                            ChannelId::LOOPBACK,
-                            Box::new(move |_| {
-                                stats.inc(StatType::Bootstrap, DetailType::TimestampReset);
-                                {
-                                    let mut guard = state.lock().unwrap();
-                                    guard.candidate_accounts.reset_last_request(&account);
-                                }
-                            }),
-                        );
-                    } else {
-                        self.block_processor.add(
-                            block,
-                            BlockSource::Bootstrap,
-                            ChannelId::LOOPBACK,
-                        );
-                    }
-                }
+                self.process_valid_blocks(query, response);
                 true
             }
             VerifyResult::NothingNew => {
-                self.stats
-                    .inc(StatType::BootstrapVerifyBlocks, DetailType::NothingNew);
-
-                {
-                    let mut guard = self.state.lock().unwrap();
-                    match guard.candidate_accounts.priority_down(&query.account) {
-                        PriorityDownResult::Deprioritized => {
-                            self.stats
-                                .inc(StatType::BootstrapAccountSets, DetailType::Deprioritize);
-                        }
-                        PriorityDownResult::Erased => {
-                            self.stats
-                                .inc(StatType::BootstrapAccountSets, DetailType::Deprioritize);
-                            self.stats.inc(
-                                StatType::BootstrapAccountSets,
-                                DetailType::PriorityEraseThreshold,
-                            );
-                        }
-                        PriorityDownResult::AccountNotFound => {
-                            self.stats.inc(
-                                StatType::BootstrapAccountSets,
-                                DetailType::DeprioritizeFailed,
-                            );
-                        }
-                        PriorityDownResult::InvalidAccount => {}
-                    }
-
-                    guard.candidate_accounts.reset_last_request(&query.account);
-                }
+                self.process_empty_response(query);
                 true
             }
             VerifyResult::Invalid => {
@@ -115,6 +45,81 @@ impl BlockAckProcessor {
                     .inc(StatType::BootstrapVerifyBlocks, DetailType::Invalid);
                 false
             }
+        }
+    }
+
+    fn process_valid_blocks(&self, query: &RunningQuery, response: &BlocksAckPayload) {
+        self.stats
+            .inc(StatType::BootstrapVerifyBlocks, DetailType::Ok);
+        self.stats.add_dir(
+            StatType::Bootstrap,
+            DetailType::Blocks,
+            Direction::In,
+            response.blocks().len() as u64,
+        );
+
+        let mut blocks = response.blocks().clone();
+
+        // Avoid re-processing the block we already have
+        assert!(blocks.len() >= 1);
+        if blocks.front().unwrap().hash() == query.start.into() {
+            blocks.pop_front();
+        }
+
+        while let Some(block) = blocks.pop_front() {
+            if blocks.is_empty() {
+                // It's the last block submitted for this account chain, reset timestamp to allow more requests
+                let stats = self.stats.clone();
+                let state = self.state.clone();
+                let account = query.account;
+                self.block_processor.add_with_callback(
+                    block,
+                    BlockSource::Bootstrap,
+                    ChannelId::LOOPBACK,
+                    Box::new(move |_| {
+                        stats.inc(StatType::Bootstrap, DetailType::TimestampReset);
+                        {
+                            let mut guard = state.lock().unwrap();
+                            guard.candidate_accounts.reset_last_request(&account);
+                        }
+                    }),
+                );
+            } else {
+                self.block_processor
+                    .add(block, BlockSource::Bootstrap, ChannelId::LOOPBACK);
+            }
+        }
+    }
+
+    fn process_empty_response(&self, query: &RunningQuery) {
+        self.stats
+            .inc(StatType::BootstrapVerifyBlocks, DetailType::NothingNew);
+
+        {
+            let mut guard = self.state.lock().unwrap();
+            match guard.candidate_accounts.priority_down(&query.account) {
+                PriorityDownResult::Deprioritized => {
+                    self.stats
+                        .inc(StatType::BootstrapAccountSets, DetailType::Deprioritize);
+                }
+                PriorityDownResult::Erased => {
+                    self.stats
+                        .inc(StatType::BootstrapAccountSets, DetailType::Deprioritize);
+                    self.stats.inc(
+                        StatType::BootstrapAccountSets,
+                        DetailType::PriorityEraseThreshold,
+                    );
+                }
+                PriorityDownResult::AccountNotFound => {
+                    self.stats.inc(
+                        StatType::BootstrapAccountSets,
+                        DetailType::DeprioritizeFailed,
+                    );
+                }
+                PriorityDownResult::InvalidAccount => {}
+            }
+
+            guard.candidate_accounts.reset_last_request(&query.account);
         }
     }
 }
